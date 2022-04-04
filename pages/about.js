0799b688342d6306f938ee4572c3d385a6b0395d
Natalia <atvnataliajn@gmail.com>
2022-04-04 16:25:09 +0200

    add tutorial

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+const About= () => {
+  return (
+    <>
+    <Head>
+      <title>Ninja List | About</title>
+      <meta name='keywords' content='ninjas'></meta>
+    </Head>
+
+    <h1>About</h1>
+    <p> lorem </p>
+    </>
+  );
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Ninja List | Home</title>
+      <meta name='keywords' content='ninjas'></meta>
+    </Head>
+    <div>
+      
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem  </p>
+      <p className={styles.text}>Lorem </p>
+      <Link href="/ninjas">
+         <a className={styles.btn}>See Ninjas LIsting</a>
+      </Link>
+    
+    </div>
+    </>
+  )
+}
